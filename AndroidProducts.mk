c7fe50e60b2f9cdf9dc0b3124506a9f8feb1d2f5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-20 08:54:37 +0200

    E7533: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: micromax Model: Micromax E7533 Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_E7533.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_E7533-user \
+    omni_E7533-userdebug \
+    omni_E7533-eng
